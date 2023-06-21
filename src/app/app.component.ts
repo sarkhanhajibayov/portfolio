c1af228971fb5733b6097322f4f048ce902ab087
Hajibayov <shacibyov@gmail.com>
2023-06-21 11:25:14 +0400

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  top: any;
+  left: any;
+  expand = false;
+
+  constructor() {}
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    this.expand = true;
+    setTimeout(() => {
+      this.expand = false;
+    }, 500);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMousemove(event: MouseEvent) {
+    this.top = event.pageY - 10 + 'px';
+    this.left = event.pageX - 10 + 'px';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidenavComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
