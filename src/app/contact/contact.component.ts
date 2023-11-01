e1b69da16c8116561b497278fd2b326d847ce5a4
Hajibayov <shacibyov@gmail.com>
2023-11-01 16:43:39 +0400

    google maps updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import * as AOS from 'aos';
-
+interface MarkerProperties {
+  position: {
+    lat: number;
+    lng: number;
+  }
+};
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,6 +18,12 @@ export class ContactComponent {
   pwdPattern = '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?!.*s).{6,12}$';
   mobnumPattern = '^((\\+91-?)|0)?[0-9]{10}$';
   emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+  center: google.maps.LatLngLiteral = { lat: 40.369259, lng:50.030410 };
+  zoom = 13
+
+  marker: MarkerProperties = 
+    { position: { lat: 40.369259, lng: 50.030410 }}// Eiffel Tower
+  ;
   constructor(private fb: FormBuilder) {}
   mainForm = this.fb.group({
     fullName: ['', [Validators.required]],
